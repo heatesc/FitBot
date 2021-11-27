3c726d99fcc51e6263e0a50008d23ae0f925b41d
Ali <alie0302@uni.sydney.edu.au>
2021-11-27 18:17:08 +1100

    added core slash commands

--- app/src/main/java/FitBot/Bot.java
+++ app/src/main/java/FitBot/Bot.java
@@ -32,15 +32,33 @@ public class Bot extends ListenerAdapter
             e.printStackTrace();
         }
         //jda.upsertCommand("track", "track a workout").queue(); // This can take up to 1 hour to show up in the client
-        //jda.getGuildById("848849465850462281").upsertCommand("track", "track a workout").addOption(OptionType.STRING, "evidence", "Provide evidence of your workout here").queue(); // WAIT TILL DISCORD API IS UPDATED TO SUPPORT ATTACHMENTS IN SLASH COMMAND OPTIONS
+        jda.getGuildById("848849465850462281").upsertCommand("track", "track a workout")
+            .addOption(OptionType.STRING, "description", "briefly describe your workout")
+            .queue(); // WAIT TILL DISCORD API IS UPDATED TO SUPPORT ATTACHMENTS IN SLASH COMMAND OPTIONS
         jda.getGuildById("848849465850462281").upsertCommand("break", "take a break if you need time off. e.g. feeling ill or overloaded with work")
             .addOption(OptionType.STRING, "reason", "What is your reason for taking time off?")
             .addOption(OptionType.INTEGER, "days", "How many days do you plan to take off? (you may end your time off early)")
             .queue(); 
+        jda.getGuildById("848849465850462281").upsertCommand("end-break", "take a break if you need time off. e.g. feeling ill or overloaded with work")
+            .queue(); 
+        jda.getGuildById("848849465850462281").upsertCommand("quit", "quit the server (potentially irreversible)")
+            .addOption(OptionType.STRING, "security", "Are you sure (yes/no)?")
+            .queue(); 
+        jda.getGuildById("848849465850462281").upsertCommand("suggestion", "provide an improvement suggestion for me")
+            .addOption(OptionType.STRING, "suggestion", "how should I improve?")
+            .queue();
+        jda.getGuildById("848849465850462281").upsertCommand("set-aim", "what is your aim for this cycle?")
+            .addOption(OptionType.STRING, "aim", "describe your aim")
+            .queue();
+        jda.getGuildById("848849465850462281").upsertCommand("change-aim", "change your aim for the cycle")
+            .addOption(OptionType.STRING, "new-aim", "describe your new aim")
+            .queue();
+        jda.getGuildById("848849465850462281").upsertCommand("get-data", "what is your aim for this cycle?")
+            .queue();
         
     }
     
-    @Override
+    /* @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
         Message msg = event.getMessage();
@@ -62,21 +80,50 @@ public class Bot extends ListenerAdapter
             //System.out.println("A");
             //System.out.println(server.getSelfMember().hasPermission(Permission.MESSAGE_WRITE));
         }
-    } 
+    }  */
 
     @Override
     public void onSlashCommand(SlashCommandEvent event)
     {
-        String username = event.getUser().getName();
-        if (!event.getName().equals("break")) return; // make sure we handle the right command
-        long time = System.currentTimeMillis();
-        //System.out.println(event.getOption("days").);
-        try {
-            event.reply(String.format("Hi %s, your time off has now started. You have %d days remaining. If you wish to end your break early use the /endbreak command.",username,event.getOption("days").getAsLong()))
-            .queue();
-        } catch (Exception e) {e.printStackTrace();}
-        //System.out.println(event.getCommandString());
-        System.out.println(Arrays.toString(event.getOptions().toArray()));
+        System.out.println(event.getName());
+        String username = event.getUser().getAsMention();
+        if (event.getName().equals("break")) {
+            try {
+                event.reply(String.format("Hi %s, your time off has now started. You have %d days remaining. If you wish to end your break early use the /endbreak command.",username,event.getOption("days").getAsLong()))
+                .queue();
+            } catch (Exception e) {e.printStackTrace();}
+        } else if (event.getName().equals("track")) {
+            try {
+                event.reply(String.format("Hi %s, your workout has been tracked.",username)).queue();
+            } catch (Exception e) {e.printStackTrace();}
+        } else if (event.getName().equals("end-break")) {
+            try {
+                event.reply(String.format("Hi %s, your break is over.",username)).queue();
+            } catch (Exception e) {e.printStackTrace();}
+        } else if (event.getName().equals("quit")) {
+            try {
+                event.reply(String.format("I haven't got the permission to kick server members, so feel free to hop off. Goodbye, %s",username)).queue();
+            } catch (Exception e) {e.printStackTrace();}
+        } else if (event.getName().equals("suggestion")) {
+            try {
+                event.reply(String.format("Thanks %s, I will take your suggestion into consideration.",username)).queue();
+            } catch (Exception e) {e.printStackTrace();}
+        } else if (event.getName().equals("set-aim")) {
+            try {
+                event.reply(String.format("Hi %s, your aim has been set!",username)).queue();
+            } catch (Exception e) {e.printStackTrace();}
+        } else if (event.getName().equals("change-aim")) {
+            try {
+                event.reply(String.format("Hi %s, your aim has been modified.",username)).queue();
+            } catch (Exception e) {e.printStackTrace();}
+        } else if (event.getName().equals("get-data")) {
+            try {
+                event.reply(String.format("Hi %s, here is your data.",username)).queue();
+            } catch (Exception e) {e.printStackTrace();}
+        } 
+
+        //System.out.println(event.getCommandString());}
+        
     }
     
 }
